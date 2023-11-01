8e4a1022f0f19bb0b6fd9bab18c5519887fbd115
skyler <skyler@skylers-MacBook-Pro.local>
2023-11-01 16:41:56 -0300

    Basic structure with react components added

---
+++ RTFilms/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import App from "./App.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+)
